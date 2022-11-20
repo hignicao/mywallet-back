845ee2e328096abd5564a4496f710bbaefba9035
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2022-11-20 03:38:58 -0300

    feat: post new transaction

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -1,5 +1,15 @@
 import { transactionsCollection } from "../database/db.js";
 
-export async function newTransaction(req, res) {}
+export async function newTransaction(req, res) {
+	const transaction = req.transaction;
+
+	try {
+		await transactionsCollection.insertOne(transaction);
+		res.sendStatus(201);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
+}
 
 export async function getTransactions(req, res) {}
